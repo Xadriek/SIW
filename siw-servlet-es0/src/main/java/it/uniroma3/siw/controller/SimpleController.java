0966fa72211a1de7c47a1e702583cc462715d7e5
Fox <72220905+Xadriek@users.noreply.github.com>
2021-05-13 14:54:05 +0200

    SpringBoot

--- siw-servlet-es0/src/main/java/it/uniroma3/siw/controller/SimpleController.java
+++ siw-servlet-es0/src/main/java/it/uniroma3/siw/controller/SimpleController.java
@@ -9,9 +9,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("serial")
 @WebServlet("/controller")
 public class SimpleController extends HttpServlet {
 	
+	@SuppressWarnings("unused")
 	private static final long serialVerionUID =1L;
        
 	protected void doGet(HttpServletRequest request, 
